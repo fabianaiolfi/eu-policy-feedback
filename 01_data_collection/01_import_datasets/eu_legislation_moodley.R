58d6a1999c4ba82a326826b8805b9a8668bb95ed
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-06-07 11:40:47 +0200

    Add Moodley dataset

---
+++ 01_data_collection/01_import_datasets/eu_legislation_moodley.R
@@ -0,0 +1,9 @@
+
+# EU legislation published between 1971-2022 ---------------------------------------
+
+# Source: https://zenodo.org/records/8174176 (retrieved 7 June 2024)
+
+# Import dataset and save as RDS
+
+# Import Metadata CSV
+moodley <- read.csv("/Volumes/iPhone_Backup_1/eu-policy-feedback-data/eu_regulations_metadata_1971_2022.csv", stringsAsFactors = FALSE)
