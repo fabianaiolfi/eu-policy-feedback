2688eef4896206a729ea034b60eb7d46e6619b5e
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-06-14 16:46:30 +0200

    Implement custom LSS

--- 02_lss/02_tbd.R
+++ 02_lss/02_tbd.R
@@ -4,9 +4,15 @@
 # Example 1
 
 # Estimate the polarity of words
+# Taking the DFM and the seed words as the only inputs, textmodel_lss() computes the polarity scores of all the words in the corpus based on their semantic similarity to the seed words. You usually do not need to change the value of k (300 by default).
 seed <- as.seedwords(data_dictionary_sentiment)
-lss <- textmodel_lss(dfmt, seeds = seed, k = 2, cache = F, 
-                     include_data = TRUE, group_data = TRUE)
+lss <- textmodel_lss(dfmt,
+                     seeds = seed,
+                     k = 2,
+                     cache = F,
+                     include_data = T,
+                     group_data = T
+                     )
 
 # Predict the polarity of documents
 dat <- docvars(lss$data)
@@ -34,5 +40,26 @@ dat2$lss <- predict(lss2, min_n = 10)
 print(nrow(dat2))
 
 
+
+#####################
+
+# Using custom dictionary
+dict <- dictionary(file = here("data", "02_lss", "l_r_dict.yml"))
+seed <- as.seedwords(dict$ideology)
+lss <- textmodel_lss(dfmt,
+                     seeds = seed,
+                     k = 300,
+                     cache = F,
+                     include_data = T,
+                     group_data = T)
+
+# Predict the polarity of documents
+# TO DO: Check if correct documents are being merged with the ID
+dat <- docvars(lss$data)
+dat$lss <- predict(lss)
+
+# Look at policies
+# sample_ceps_eurlex$act_raw_text[37]
+sample_ceps_eurlex$CELEX[886]
 
 
