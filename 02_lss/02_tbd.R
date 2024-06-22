3477202406bf6c8d7b00966ec78e9580f572e5de
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-06-22 14:21:28 +0200

    LSS exploration

--- 02_lss/02_tbd.R
+++ 02_lss/02_tbd.R
@@ -44,7 +44,7 @@ print(nrow(dat2))
 #####################
 
 # Using custom dictionary
-dict <- dictionary(file = here("data", "02_lss", "l_r_dict.yml"))
+dict <- dictionary(file = here::here("data", "02_lss", "l_r_dict.yml"))
 seed <- as.seedwords(dict$ideology)
 lss <- textmodel_lss(dfmt,
                      seeds = seed,
@@ -61,5 +61,67 @@ dat$lss <- predict(lss)
 # Look at policies
 # sample_ceps_eurlex$act_raw_text[37]
 sample_ceps_eurlex$CELEX[886]
+
+#####################
+# Clean corpus
+# https://tutorials.quanteda.io/machine-learning/lss/
+
+# tokenize text corpus and remove various features
+corp_sent <- corpus_reshape(sample_ceps_eurlex_corpus, to =  "sentences")
+toks_sent <- corp_sent %>% 
+  tokens(remove_punct = TRUE, remove_symbols = TRUE, 
+         remove_numbers = TRUE, remove_url = TRUE) %>% 
+  tokens_remove(stopwords("en", source = "marimo")) %>% 
+  # remove tokens that are shorter than 3 characters
+  tokens_remove(min_nchar = 3) %>%
+  tokens_remove(c("article", "shall", "annex", "commission", "decision", "member", "european", "state*", "measure*", "regard", "directive"))
+
+# create a document feature matrix from the tokens object
+dfmat_sent <- toks_sent %>% 
+  dfm() %>% 
+  dfm_remove(pattern = "") %>% 
+  dfm_trim(min_termfreq = 5)
+
+topfeatures(dfmat_sent, 20)
+
+# identify context words 
+context_words <- char_context(toks_sent, pattern = "authority", p = 0.05)
+context_words
+
+# Create LSS Model
+tmod_lss <- textmodel_lss(dfmat_sent,
+                          seeds = seed,
+                          k = 300,
+                          simil_method = "ejaccard",
+                          cache = F
+                          )
+
+head(coef(tmod_lss), 20) # ideologically left words
+tail(coef(tmod_lss), 20) # ideologically right words
+
+
+
+#####################
+# Selection of seed words
+# https://koheiw.github.io/LSX/articles/pkgdown/seedwords.html
+
+# print(lss)
+print(tmod_lss$seeds)
+print(tmod_lss$seeds_weighted)
+
+bs_term <- bootstrap_lss(tmod_lss, mode = "terms")
+# knitr::kable(head(bs_term, 10))
+head(bs_term, 10)
+
+bs_coef <- bootstrap_lss(tmod_lss, mode = "coef")
+#knitr::kable(head(bs_coef, 10), digits = 3)
+head(bs_coef, 10)
+
+# filter by row name
+bs_coef[rownames(bs_coef) %in% c("solidarity"),]
+
+
+
+
 
 
