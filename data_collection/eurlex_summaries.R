ef608d72b3853fc73504251fa06abd725d1f2ee0
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-07-11 15:02:55 +0200

    Scraping exploration: Which policies have a summary?

--- data_collection/eurlex_summaries.R
+++ data_collection/eurlex_summaries.R
@@ -13,7 +13,7 @@ ceps_eurlex_dir_reg <- ceps_eurlex_dir_reg %>% select(CELEX)
 
 # Add EURLEX URL to directives and regulations subset
 ceps_eurlex_dir_reg <- ceps_eurlex_dir_reg %>% 
-  left_join(select(ceps_eurlex, CELEX, Date_document, Act_type, Eurlex_link), by = "CELEX")
+  left_join(select(ceps_eurlex, CELEX, Date_document, Act_type, Eurlex_link, Status), by = "CELEX")
 
 # Create summaries link 
 ceps_eurlex_dir_reg_summaries <- ceps_eurlex_dir_reg %>% 
@@ -24,9 +24,10 @@ ceps_eurlex_dir_reg_summaries <- ceps_eurlex_dir_reg %>%
 
 # For testing purposes: Create sample of ceps_eurlex_dir_reg
 ceps_eurlex_dir_reg_summaries <- ceps_eurlex_dir_reg_summaries %>%
-  dplyr::filter(Date_document >= "2019-01-01") %>% 
-  dplyr::filter(Act_type == "Directive") %>% 
-  slice_sample(n = 10, replace = F)
+  # dplyr::filter(Date_document >= "2015-01-01") %>% 
+  # Directives seem to be more likely to have summaries
+  dplyr::filter(Act_type == "Directive") %>%
+  slice_sample(n = 100, replace = F)
 
 
 # Scrape Summaries ----------------------------------------------------------------
@@ -65,15 +66,35 @@ counter <- 1
 ceps_eurlex_dir_reg_summaries$eurlex_summary <- sapply(all_links, function(link) {
   eurlex_summary <- scrape_text(link, user_agent_string)
   message("Processed file ", counter, " of ", length(all_links), "\n")
-  Sys.sleep(3) # Be polite and do not overload the server
+  Sys.sleep(1) # Be polite and do not overload the server
   counter <<- counter + 1
   return(eurlex_summary)
 })
+
+saveRDS(ceps_eurlex_dir_reg_summaries, file = here("data", "data_collection", "ceps_eurlex_dir_reg_summaries_xx.rds"))
+# ceps_eurlex_dir_reg_summaries <- readRDS(file = here("data", "data_collection", "ceps_eurlex_dir_reg_summaries_xx.rds"))
+
+# temp_df <- ceps_eurlex_dir_reg_summaries %>% 
+#   mutate(eurlex_summary = as.character(eurlex_summary)) %>% 
+#   # replace "NA" string with NA
+#   mutate(eurlex_summary = ifelse(eurlex_summary == "NA", NA, eurlex_summary)) %>% 
+#   mutate(eurlex_summary = ifelse(eurlex_summary == "character(0)", NA, eurlex_summary)) %>%
+#   drop_na(eurlex_summary) %>% 
+#   mutate(eurlex_summary_clean = str_squish(eurlex_summary)) %>% # Remove excessive whitespace
+#   mutate(eurlex_summary_clean = str_replace_all(eurlex_summary_clean, "\n", " ")) %>% # Replace newline characters with space
+#   mutate(eurlex_summary_clean = str_replace_all(eurlex_summary_clean, "\\s{2,}", " ")) %>% # Replace multiple spaces with a single space
+#   mutate(eurlex_summary_clean = str_trim(eurlex_summary_clean)) %>% 
+#   left_join(select(ceps_eurlex, -Date_document, -Act_type, -Eurlex_link, -Status), by = "CELEX")
 
 
 # Clean Up Summaries ----------------------------------------------------------------
 
 ceps_eurlex_dir_reg_summaries <- ceps_eurlex_dir_reg_summaries %>% 
+  mutate(eurlex_summary = as.character(eurlex_summary)) %>% 
+  # replace "NA" and "character(0)" string with NA
+  mutate(eurlex_summary = ifelse(eurlex_summary == "NA", NA, eurlex_summary)) %>% 
+  mutate(eurlex_summary = ifelse(eurlex_summary == "character(0)", NA, eurlex_summary)) %>%
+  drop_na(eurlex_summary) %>% 
   mutate(eurlex_summary_clean = str_squish(eurlex_summary)) %>% # Remove excessive whitespace
   mutate(eurlex_summary_clean = str_replace_all(eurlex_summary_clean, "\n", " ")) %>% # Replace newline characters with space
   mutate(eurlex_summary_clean = str_replace_all(eurlex_summary_clean, "\\s{2,}", " ")) %>% # Replace multiple spaces with a single space
@@ -83,4 +104,4 @@ ceps_eurlex_dir_reg_summaries <- ceps_eurlex_dir_reg_summaries %>%
 
 # Save to file ----------------------------------------------------------------
 
-# saveRDS(ceps_eurlex_dir_reg_summaries, file = here("data", "data_collection", "ceps_eurlex_dir_reg_summaries_240711.rds"))
+saveRDS(ceps_eurlex_dir_reg_summaries, file = here("data", "data_collection", "ceps_eurlex_dir_reg_summaries_240711_04.rds"))
