7761570df00056d070d354e74c15a6624a7b7a00
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-08-23 18:37:54 +0200

    Refine data collection

--- lss/seed_word_wordscores.R
+++ lss/seed_word_wordscores.R
@@ -1,28 +1,63 @@
 
 # Seed Words: Wordscores Approach -----------------------
 
-## Get Manifesto Data -----------------
+## Get Manifesto Data -----------------------
 
+parties_MPDataset <- read.csv(file = here("lss", "parties_MPDataset_MPDS2024a.csv"))
+
+# Documentation:
+# https://manifesto-project.wzb.eu/information/documents/translation
+# https://manifesto-project.wzb.eu/down/tutorials/firststepsmanifestoR.html#downloading-documents-from-the-manifesto-corpus
+
+# Set API key
 mp_setapikey("manifesto_apikey.txt")
 
-corpus <- mp_corpus_df(date == 202103, translation = "en")
+english_translation <- mp_availability(TRUE) %>% 
+  dplyr::filter(translation_en == TRUE | language == "english")
 
+english_translation_manifesto_corpus <- mp_corpus_df(english_translation, translation = "en")
+
+saveRDS(english_translation_manifesto_corpus, file = here("lss", "english_translation_manifesto_corpus.rds"))
+# english_translation_manifesto_corpus <- readRDS(file = here("lss", "english_translation_manifesto_corpus.rds"))
+
+
+## Create Manifesto Data Subset --------------------
+# Only include manifestos from Western Europe between 1989 and 2024
+
+# Add country to each row
+english_translation_manifesto_corpus <- english_translation_manifesto_corpus %>% 
+  left_join(select(parties_MPDataset, party, countryname), by = "party")
+
+w_europe <- c("Sweden", "Norway", "Denmark", "Finland", "Belgium", "Netherlands",
+               "Luxembourg", "France", "Italy", "Spain", "Greece", "Portugal",
+               "Germany", "Austria", "Switzerland", "United Kingdom", "Ireland", "Cyprus")
+
+subset_manifesto_corpus <- english_translation_manifesto_corpus %>% 
+  dplyr::filter(date > 198900) %>% 
+  dplyr::filter(countryname %in% w_europe)
+
+
+#####
+
+
+
+# corpus <- mp_corpus_df(date == 202103, translation = "en")
 
 doclist <- mp_metadata(TRUE) %>% dplyr::filter(translation_en == TRUE | language == "english")
 
-w_europe <- c("swedish", "norwegian", "danish", "finnish", "french", "dutch", "german", "italian",
-              "spanish", "greek", "portuguese", "english")
+# w_europe <- c("swedish", "norwegian", "danish", "finnish", "french", "dutch", "german", "italian",
+#               "spanish", "greek", "portuguese", "english")
 
-my_doclist <- doclist %>% 
+w_europe <- doclist %>% 
   dplyr::filter(date > 198900) %>% 
   dplyr::filter(language %in% w_europe)
   
 
-test_df <- my_doclist %>% slice_sample(n = 10)
+#test_df <- my_doclist %>% slice_sample(n = 10)
 
 #corpus_test <- mp_corpus_df(ids = (manifesto_id == "160956_201905"))
 
-english_annotated <- mp_availability(TRUE) %>% dplyr::filter(annotations == TRUE & language == "english")
+# english_annotated <- mp_availability(TRUE) %>% dplyr::filter(annotations == TRUE & language == "english")
 english_translation <- mp_availability(TRUE) %>% dplyr::filter(translation_en == TRUE | language == "english")
 
 corpus_test <- mp_corpus(english_translation)
@@ -33,6 +68,8 @@ saveRDS(corpus_test_df, file = here("lss", "corpus_test_df.rds"))
 
 corpus_test_df_3 <- mp_corpus_df(english_translation, translation = "en")
 saveRDS(corpus_test_df_3, file = here("lss", "corpus_test_df_3.rds"))
+
+max(corpus_test_df_3$date)
 
 ###################
 
