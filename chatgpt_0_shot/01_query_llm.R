f5dca0d3bdec956a880e0a8de4a2af2d70bbfd64
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-08-30 09:13:28 +0200

    Init 0-shot approach based on ranking script

---
+++ chatgpt_0_shot/01_query_llm.R
@@ -0,0 +1,153 @@
+
+# Load Data ---------------------------------------------------------------
+
+all_dir_reg_summaries <- readRDS(file = here("data", "data_collection", "all_dir_reg_summaries.rds"))
+all_dir_reg_summaries <- all_dir_reg_summaries %>% head(10)
+
+
+# # Indexing ---------------------------------------------------------------
+# 
+# # Create a dataframe that lists two different CELEX IDs per row
+# celex_index <- ceps_eurlex_dir_reg_summaries %>% 
+#   select(CELEX) %>% # Only select CELEX
+#   rename(CELEX_1 = CELEX)
+# celex_index <- bind_rows(replicate(3, celex_index, simplify = F)) # Repeat same row 3 times
+# 
+# # Function to shuffle and ensure no duplicates in the same row
+# create_shuffled_column <- function(df) {
+#   original <- df$CELEX_1
+#   shuffled <- sample(original)
+#   
+#   # Check and reshuffle until no duplicates are on the same row
+#   while(any(original == shuffled)) {
+#     shuffled <- sample(original)
+#   }
+#   
+#   df$CELEX_2 <- shuffled
+#   return(df)
+# }
+# 
+# # Apply the function to create the new column
+# celex_index <- create_shuffled_column(celex_index)
+
+
+# Create Prompt Dataframe ---------------------------------------------------------------
+
+system_prompt <- "You are an expert in European Union policies. Answer questions and provide information based on that expertise.\n\n"
+
+
+## Prompt for economic *left* ---------------------------------------------------------------
+
+prompt <- "I’m going to show you a summary of an EU policy. Please score the policy on a scale of 0 to 10. 0 represents economic left-wing policies, such as government intervention in the economy, redistribution of wealth, social welfare programs, progressive taxation, regulation of markets, and support for labor rights. 10 represents economic right-wing policies such as free market capitalism, deregulation, lower taxes, privatization, reduced government spending, and individual financial responsibility. Please only return the score. Here’s the summary:\n\n"
+
+prompt_df <- all_dir_reg_summaries %>% 
+  # left_join(ceps_eurlex_dir_reg_summaries, by = c("CELEX_1" = "CELEX")) %>% 
+  # rename(policy_summary_1 = eurlex_summary_clean) %>% 
+  # left_join(ceps_eurlex_dir_reg_summaries, by = c("CELEX_2" = "CELEX")) %>% 
+  # rename(policy_summary_2 = eurlex_summary_clean) %>% 
+  # mutate(id_var = paste0(CELEX_1, "_", CELEX_2)) %>% # ID for each row
+  mutate(prompt_role_var = "user") %>% # Set role
+  # Put together prompt
+  mutate(prompt_content_var = paste0(system_prompt, 
+                                     prompt, 
+                                     # "Policy Summary 1:\n",
+                                     # policy_summary_1, "\n\n",
+                                     # "Policy Summary 2:\n",
+                                     # policy_summary_2, "\n\n",
+                                     eurlex_summary_clean)) %>% 
+  # Count number of tokens in prompt (1 token ~= 4 chars in English, see https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them)
+  # Check API limits: https://platform.openai.com/settings/organization/limits
+  mutate(prompt_token_len = nchar(prompt_content_var) / 4)
+
+# Save prompt_df to file
+# timestamp <- Sys.time()
+# formatted_timestamp <- format(timestamp, "%Y%m%d_%H%M%S")
+# file_name <- paste0("prompt_df_", formatted_timestamp, ".rds")
+# saveRDS(prompt_df, file = here("data", "ranking", file_name))
+
+
+# ## Prompt for economic *right* ---------------------------------------------------------------
+# 
+# prompt_start <- "I have two summaries of EU policies, and I need to determine which policy is more economically right-leaning. Please analyze the summaries based on principles commonly associated with economically right policies, such as free market capitalism, deregulation, lower taxes, privatization, reduced government spending, and individual financial responsibility.\n\n"
+# prompt_end <- "Which policy is more economically right? Please return only '1' or '2'."
+# 
+# prompt_df <- celex_index %>% 
+#   left_join(ceps_eurlex_dir_reg_summaries, by = c("CELEX_1" = "CELEX")) %>% 
+#   rename(policy_summary_1 = eurlex_summary_clean) %>% 
+#   left_join(ceps_eurlex_dir_reg_summaries, by = c("CELEX_2" = "CELEX")) %>% 
+#   rename(policy_summary_2 = eurlex_summary_clean) %>% 
+#   mutate(id_var = paste0(CELEX_1, "_", CELEX_2)) %>% # ID for each row
+#   mutate(prompt_role_var = "user") %>% # Set role
+#   # Put together prompt
+#   mutate(prompt_content_var = paste0(system_prompt, 
+#                                      prompt_start, 
+#                                      "Policy Summary 1:\n",
+#                                      policy_summary_1, "\n\n",
+#                                      "Policy Summary 2:\n",
+#                                      policy_summary_2, "\n\n",
+#                                      prompt_end)) %>% 
+#   # Count number of tokens in prompt (1 token ~= 4 chars in English, see https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them)
+#   # Check API limits: https://platform.openai.com/settings/organization/limits
+#   mutate(prompt_token_len = nchar(prompt_content_var) / 4)
+# 
+# # Save prompt_df to file to assure reproducability despite randomness in celex_index
+# timestamp <- Sys.time()
+# formatted_timestamp <- format(timestamp, "%Y%m%d_%H%M%S")
+# file_name <- paste0("prompt_df_", formatted_timestamp, ".rds")
+# saveRDS(prompt_df, file = here("data", "ranking", file_name))
+
+
+
+# Query ChatGPT ---------------------------------------------------------------
+
+# https://github.com/ben-aaron188/rgpt3?tab=readme-ov-file#getting-started
+# Setup and test ChatGPT API
+rgpt_authenticate("access_key.txt")
+# rgpt_test_completion(verbose = T)
+
+# Create small sample of prompt_df for testing
+# prompt_df <- head(prompt_df, 3)
+
+chatgpt_output <- rgpt(
+  prompt_role_var = prompt_df$prompt_role_var,
+  prompt_content_var = prompt_df$prompt_content_var,
+  param_seed = project_seed, # Defined in .Rprofile
+  id_var = prompt_df$CELEX,
+  param_output_type = "complete",
+  param_model = "gpt-4o-mini-2024-07-18",
+  param_max_tokens = 5,
+  param_temperature = 0,
+  param_n = 1)
+
+# Save output to file
+# Get and format the current timestamp to prevent overwriting files when saving RData locally
+timestamp <- Sys.time()
+formatted_timestamp <- format(timestamp, "%Y%m%d_%H%M%S")
+file_name <- paste0("chatgpt_output_df_", formatted_timestamp, ".rds")
+saveRDS(chatgpt_output, file = here("data", "chatgpt_0_shot", file_name))
+
+
+# Process ChatGPT output ---------------------------------------------------------------
+
+chatgpt_output <- readRDS(file = here("data", "ranking", "chatgpt_output_df_20240712_143525.rds"))
+prompt_df <- readRDS(file = here("data", "ranking", "prompt_df_20240712_142655.rds"))
+
+# Convert output to dataframe
+temp_df <- chatgpt_output[[1]]
+temp_df <- temp_df %>% 
+  select(id, gpt_content) %>% 
+  rename(chatgpt_answer = gpt_content) %>% 
+  distinct(id, .keep_all = T) # This shouldn't be necessary if CELEX_1 and CELEX_2 are never identical
+
+# Merge ChatGPT answer with prompt_df
+prompt_df <- prompt_df %>% left_join(temp_df, by = c("CELEX" = "id"))
+
+# # Prepare dataframe for ranking
+# ranking_df <- prompt_df %>% 
+#   select(CELEX_1, CELEX_2, chatgpt_answer) %>% 
+#   mutate(chatgpt_answer = as.numeric(chatgpt_answer)) %>% 
+#   # Adjust accordingly
+#   # mutate(more_left = case_when(chatgpt_answer == 1 ~ CELEX_1,
+#   #                              chatgpt_answer == 2 ~ CELEX_2))
+#   mutate(more_right = case_when(chatgpt_answer == 1 ~ CELEX_1,
+#                                chatgpt_answer == 2 ~ CELEX_2))
