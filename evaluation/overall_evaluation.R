d67f66af34b2b2afad0b33d7bcb0be40699bfb40
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-19 17:51:32 +0200

    Continue implementing z-score

--- evaluation/overall_evaluation.R
+++ evaluation/overall_evaluation.R
@@ -13,12 +13,26 @@ all_dir_reg <- all_dir_reg %>% slice_sample(n = 1000)
 # source(here("evaluation", "policy_area_subj_matter.R")) # Run script
 policy_area_subj_matter <- readRDS(here("data", "evaluation", "policy_area_subj_matter.rds")) # Load data
 
+# Import Calculated measurements
 glove_polarity_scores_all_dir_reg_econ <- readRDS(here("data", "lss", "glove_polarity_scores_all_dir_reg_econ.rds"))
+glove_polarity_scores_all_dir_reg_social <- readRDS(here("data", "lss", "glove_polarity_scores_all_dir_reg_social.rds"))
+hix_hoyland_data <- readRDS(here("existing_measurements", "hix_hoyland_2024", "hix_hoyland_data.rds"))
 
 # Add Subject Matter to all_dir_reg
 ceps_eurlex <- readRDS(here("data", "data_collection", "ceps_eurlex.rds"))
 ceps_eurlex <- ceps_eurlex %>% select(CELEX, Subject_matter)
 all_dir_reg <- all_dir_reg %>% left_join(ceps_eurlex, by = "CELEX")
+
+
+## Preprocess Data -----------------------
+
+nanou_2017_lrscale3 <- nanou_2017_lrscale3 %>% 
+  # Perform standardization of data (z-scoring)
+  mutate(lrscale3_avg_z_score = standardize(lrscale3_avg))
+
+glove_polarity_scores_all_dir_reg_econ <- glove_polarity_scores_all_dir_reg_econ %>% 
+  # Perform standardization of data (z-scoring)
+  mutate(avg_lss_econ_z_score = standardize(avg_glove_polarity_scores))
 
 
 ## Connect a Law's Subject Matter with the Broad Policy Area from Nanou 2017 -----------------------
@@ -67,7 +81,14 @@ broad_policy_avg_df <- all_dir_reg %>%
   separate_rows(broad_policy_area, sep = "; ") %>% # Place each Broad Policy Area on its own row
   # Add calculated scores
   left_join(glove_polarity_scores_all_dir_reg_econ, by = "CELEX") %>% 
-  rename(lss_econ = avg_glove_polarity_scores)# %>% 
+  rename(lss_econ = avg_glove_polarity_scores) %>% 
+  left_join(glove_polarity_scores_all_dir_reg_social, by = "CELEX") %>% 
+  rename(lss_social = avg_glove_polarity_scores) %>% 
+  left_join(hix_hoyland_data, by = "CELEX") %>% 
+  rename(hix_hoyland_RoBERT_left_right = RoBERT_left_right) %>% 
+  rename(hix_hoyland_bakker_hobolt_econ = bakker_hobolt_econ) %>% 
+  rename(hix_hoyland_bakker_hobolt_social = bakker_hobolt_social) %>% 
+  rename(hix_hoyland_cmp_left_right = cmp_left_right)
   ## Add other scores here later ##
 
 # Calcualate averages based on time periods
@@ -80,18 +101,24 @@ broad_policy_avg_df <- broad_policy_avg_df %>%
     year %in% 2006:2010 ~ "2006-2010",
     year %in% 2011:2014 ~ "2011-2014",
     year %in% 2014:2024 ~ "2014-2024",
-    TRUE ~ NA_character_)) %>% 
+    TRUE ~ NA_character_))# %>% 
   drop_na(period) %>%
   group_by(broad_policy_area, period) %>%
-  summarise(avg_lss_econ = mean(lss_econ, na.rm = T), .groups = "drop") %>% 
-  # Perform a standardization of data (z-scoring)
-  mutate(avg_lss_econ_z_score = standardize(avg_lss_econ))
+  summarise(avg_lss_econ = mean(lss_econ, na.rm = T), .groups = "drop")
 
 
 ## CONTINUE HERE ##
-# - normalise different scores
+# - normalise different scores: calcualte value BEFORE calculating average!
 # - add all calculated scores (both LSS, Hix Hoyland, etc)
 # - compare with Nanou 2017 (ie expert survey)
 
 temp_df <- broad_policy_avg_df %>% 
   left_join(nanou_2017_lrscale3, by = c("broad_policy_area", "period"))
+
+
+plot(temp_df$avg_lss_econ_z_score, temp_df$lrscale3_avg_z_score)
+
+
+
+
+
