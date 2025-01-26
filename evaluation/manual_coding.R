bdc1b1b3cde3026469c1a3a4c53b888689f0c55e
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-01-26 16:51:34 +0100

    Finalise correlation between human coders

--- evaluation/manual_coding.R
+++ evaluation/manual_coding.R
@@ -9,8 +9,15 @@ manual_coding <- read_sheet("https://docs.google.com/spreadsheets/d/1DUyJZvo6g-J
 
 # Create sample ----------------------
 
+manual_sample <- c("31990R0153",
+                   "31995R2420",
+                   "31990R1216",
+                   "32010R0679",
+                   "31993R2920",
+                   "31991R3235")
+
 all_data_sample <- all_data %>%
-  slice_sample(n = 6) %>% 
+  dplyr::filter(CELEX %in% manual_sample) %>% 
   select(-date, -act_string, -act_type, -act_summary, -eurlex_policy_area, -nanou_broad_policy_area, -contains("summary"))
 
 #1 31990R0153: https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:31990R0153&qid=1737798685022
@@ -87,38 +94,52 @@ manual_coding_social_lr <- manual_coding %>%
 # Measure Intraclass Correlation -----------------------
 # Source: https://www.datanovia.com/en/lessons/intraclass-correlation-coefficient-in-r/
 
-icc(manual_coding_general_lr, model = "twoway", type = "agreement", unit = "single")
-icc(manual_coding_econ_relevance, model = "twoway", type = "agreement", unit = "single")
-icc(manual_coding_social_relevance, model = "twoway", type = "agreement", unit = "single")
-icc(manual_coding_economic_lr, model = "twoway", type = "agreement", unit = "single")
-icc(manual_coding_social_lr, model = "twoway", type = "agreement", unit = "single")
+general_lr <- icc(manual_coding_general_lr, model = "twoway", type = "agreement", unit = "single")$value
+econ_relevance <- icc(manual_coding_econ_relevance, model = "twoway", type = "agreement", unit = "single")$value
+social_relevance <- icc(manual_coding_social_relevance, model = "twoway", type = "agreement", unit = "single")$value
+economic_lr <- icc(manual_coding_economic_lr, model = "twoway", type = "agreement", unit = "single")$value
+social_lr <- icc(manual_coding_social_lr, model = "twoway", type = "agreement", unit = "single")$value
 
+message("General LR: ", round(general_lr, 2))
+message("Economic Relevance: ", round(econ_relevance, 2))
+message("Social Relevance: ", round(social_relevance, 2))
+message("Economic LR: ", round(economic_lr, 2))
+message("Social LR: ", round(social_lr, 2))
+
+# Interpretation
+# below 0.50: poor
+# between 0.50 and 0.75: moderate
+# between 0.75 and 0.90: good
+# above 0.90: excellent
 
 # Manual Coding vs Machines -----------------------
 
-# Average human coders score
-avg_manual_coding <- manual_coding %>% 
-  dplyr::group_by(Measurement) %>% 
-  dplyr::summarise(mean = mean(Score))
+# Attention: Do scores need to be normalised first?
 
-
-
-
-
-
-
-#######################
-
-manual_sample <- c("31990R0153",
-                   "31995R2420",
-                   "31990R1216",
-                   "32010R0679",
-                   "31993R2920",
-                   "31991R3235")
-
-temp <- readRDS(here("data", "data_collection", "all_data.rds"))
-temp_sample <- temp %>% 
-  dplyr::filter(CELEX %in% manual_sample) %>% 
-  select(-date, -act_string, -act_type, -act_summary, -eurlex_policy_area, -nanou_broad_policy_area, -contains("summary"))
-
-write.csv(temp_sample, file = "/Users/aiolf1/Library/CloudStorage/Dropbox/Work/240304 Qualtrics Giorgio/00 Admin/Meetings/250129 Manual Coding/machine_coding.csv", row.names = F)
+# # Average human coders score
+# avg_manual_coding <- manual_coding %>% 
+#   dplyr::filter(Measurement != "Economic relevance" & Measurement != "Social relevance") %>% 
+#   dplyr::group_by(Measurement, CELEX) %>% 
+#   dplyr::summarise(manual_score = mean(Score)) %>% 
+#   dplyr::ungroup()
+# 
+# avg_manual_coding_general_lr <- avg_manual_coding %>% 
+#   dplyr::filter(Measurement == "General LR") %>% 
+#   dplyr::select(-Measurement) %>% 
+#   left_join(all_data_sample, by = "CELEX") %>% 
+#   tibble::column_to_rownames(var = "CELEX") %>% 
+#   select(manual_score, hix_hoyland_robert_left_right_preamble)
+# 
+# human_vs_machines_general_lr <- icc(avg_manual_coding_general_lr, model = "twoway", type = "agreement", unit = "single")$value
+# 
+# avg_manual_coding_econ_lr <- avg_manual_coding %>% 
+#   dplyr::filter(Measurement == "Economic LR") %>% 
+#   dplyr::select(-Measurement) %>% 
+#   left_join(all_data_sample, by = "CELEX") %>% 
+#   tibble::column_to_rownames(var = "CELEX") %>% 
+#   select(manual_score, hix_hoyland_bakker_hobolt_econ_preamble)
+# 
+# human_vs_machines_econ_lr <- icc(avg_manual_coding_econ_lr, model = "twoway", type = "agreement", unit = "single")$value
+# 
+# message("Human vs Machines General LR: ", round(human_vs_machines_general_lr, 2))
+# message("Human vs Machines Economic LR: ", round(human_vs_machines_econ_lr, 2))
